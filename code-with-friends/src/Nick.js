7ef45daf954f7966a0b4e517d8af1f3d33411c6c
nickorfitelli <nicholas.orfitelli.1@us.af.mil>
2020-07-25 02:02:01 -0400

    adding nick component

---
+++ code-with-friends/src/Nick.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Nick() {
+    return (
+        <div>
+            Nicks Portion
+            <button>Hello World</button>
+            <button>Bye World</button>
+        </div>
+    )
+}
+
+export default Nick
