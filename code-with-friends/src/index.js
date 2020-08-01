bc320e934ebab7b94354f0264f303f6c64b6cc80
Marlon Cabugwas <akoangfilipino@gmail.com>
2020-07-31 19:51:32 -0700

    Added my module files

--- code-with-friends/src/index.js
+++ code-with-friends/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Marlon from './Marlon';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Marlon />
   </React.StrictMode>,
   document.getElementById('root')
 );
